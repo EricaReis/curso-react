c6ebb620931e064e565326ed69559586d7d8c42f
EricaReis <ericareis67@outlook.com>
2020-07-13 10:05:05 -0300

    select test

--- src/MyForm.js
+++ src/MyForm.js
@@ -5,14 +5,25 @@ class MyForm extends Component{
         super(props);
 
         this.state = {
-            name: ''
+            name: '',
+            fruit: 'orange',
+            message: ''
         }
+
+        this.fruits = [
+            {'name': 'Apple', 'value': 'apple'},
+            {'name': 'Orange', 'value': 'orange'}, 
+            {'name': 'Banana', 'value': 'banana'}  
+        ];
 
         this.handleChange = this.handleChange.bind(this);
     }
     handleChange(event){
+        const target = event.target,
+            value = target.type === 'checkbox' ? target.checked : target.value,
+            name = target.name
         this.setState({
-            name: event.target.value
+            [name]: event.target.value
         })
     }
 
@@ -20,10 +31,28 @@ class MyForm extends Component{
         const { state } = this;
         return(
             <form>
+                <div>
                 <label>
                     Name:
                     <input type="text" name="name" value={state.name} onChange={this.handleChange}/>
                 </label>
+                </div>
+                <div>
+                <label>
+                    Fruit:
+                    <select value={state.fruit} name="fruit" onChange={this.handleChange}>
+                        {
+                            this.fruits.map(fruit => <option value={fruit.value}>{fruit.name}</option>)
+                        }
+                    </select>
+                </label>
+                </div>
+                <div>
+                <label>
+                    Message:
+                    <textarea name="message" value={state.message} onChange={this.handleChange}/>
+                </label>
+                </div>
                 <input type="submit" value="Enviar"/>
             </form>
         )
